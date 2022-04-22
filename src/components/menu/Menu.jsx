b7e7837ef3e68a226777a3c14428d850c68c9edc
Shubham Pandey <mr.pshubham@gmail.com>
2022-04-22 19:32:38 +0530

    Topbar complete

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './menu.scss'
+
+export default function Menu() {
+  return (
+    <div className='menu'>
+        <ul>
+            <li><a href='#intro'>HOme</a></li>
+            <li><a href='#intro'>HOme</a></li>
+            <li><a href='#intro'>HOme</a></li>
+            <li><a href='#intro'>HOme</a></li>
+            <li><a href='#intro'>HOme</a></li>
+        </ul>
+    </div>
+  )
+}
